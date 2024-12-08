b32f5d31562019f8061f114a7f56613953c699af
Dimas <dimasat15@gmail.com>
2024-12-08 17:01:24 +0900

    finalize experiment

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from trainers import Trainer
 
 config = {
     # Training
-    "epochs": 100,
-    "learning_rate": 1e-3,
-    "weight_decay": 1e-5,
-    "patience": 15,
+    "epochs": 1000,
+    "learning_rate": 5e-2,
+    "weight_decay": 1e-4,
+    "patience": 50,
     "min_delta": 1e-4,
     "sched_factor": 0.5,
-    "sched_patience": 5,
+    "sched_patience": 10,
 
     # Model
     "layers": 3,
@@ -25,22 +25,27 @@ config = {
 
     # Utils
     "seed": 15,
-    "print_every": 5,
-    "val_every": 10,
+    "print_every": 10,
+    "val_every": 25,
     "save_path": './Experiments/'
 }
 
 if __name__ == '__main__':
     R, P, Q = get_sparse_matrices(config["path"], config["discrete_levels"])
-    trainer = Trainer(R, P, Q, config)
-    train_losses, val_losses = trainer.train()
-    model = trainer.model
+    for i in [False, True]:
+        config["weighted"] = i
+        print("\nWeighted: ", i)
+        for seed in [15, 24, 35]:
+            print("\nSeed: ", seed)
+            config["seed"] = seed
+            trainer = Trainer(R, P, Q, config)
+            train_losses, val_losses = trainer.train()
+            model = trainer.model
 
-    plt.figure(figsize=(10, 5))
-    plt.plot(train_losses, label='Train loss')
-    plt.plot(val_losses, label='Validation loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig(str(config["save_path"]) + 'loss.png')
-    plt.show()
+            plt.figure(figsize=(10, 5))
+            plt.plot(train_losses, label='Train loss')
+            plt.plot(val_losses, label='Validation loss')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.legend()
+            plt.savefig(str(config["save_path"]) + 'loss.png')
